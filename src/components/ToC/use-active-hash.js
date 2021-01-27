f15ce15b92e0a10bc2eaf02f0602c0221c60e4ef
Jinyan Yu <yujinyan@outlook.com>
2021-01-27 20:33:32 +0800

    ToC on desktop (#11)
    * working toc impl
    
    * alternative impl
    
    * cleanup
    
    * speed up transition
    
    * smaller dark mode toggle
    
    * fixes
    
    * fix ssr
    
    * cleanup

---
+++ src/components/ToC/use-active-hash.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+export const useActiveHash = (itemIds, rootMargin = undefined) => {
+  const isSSR = typeof window === "undefined"
+  const [activeHash, setActiveHash] = useState(``)
+
+  useEffect(() => {
+    if (isSSR) return []
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            setActiveHash(entry.target.id)
+          }
+        })
+      },
+      { rootMargin: rootMargin || `0% 0% -80% 0%` }
+    )
+
+    itemIds.forEach(id => {
+      // handle Chinese characters
+      const decoded = decodeURI(id)
+      console.log(`id is ${decoded}`)
+      observer.observe(document.getElementById(decoded))
+    })
+
+    return () => {
+      itemIds.forEach(id => {
+        const element = document.getElementById(id)
+        element && observer.unobserve(element)
+      })
+    }
+  }, [])
+
+  return activeHash
+}
