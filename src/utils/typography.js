a726eb8e1dad982bb4463535b7541a4bc8e02136
Jinyan Yu <yujinyan@outlook.com>
2021-02-19 21:00:43 +0800

    Google Fonts -> Adobe Fonts, enable `palt` (#28)
    * replace Google Fonts with Adobe Fonts, add palt
    
    * update typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,13 +2,19 @@ import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 import './global.css'
 
+const SourceHanSerif = "source-han-serif-sc"
+const SourceHanSans = "source-han-sans-simplified-c"
+
 Wordpress2016.overrideThemeStyles = ({ rhythm }, _, styles) => {
+  styles.body.fontFeatureSettings += `, "palt"` // 标点挤压
+
   // Fix scrolling main content when mobile menu is open.
   delete styles.html.overflowY
+
   return {
     "a.gatsby-resp-image-link": { boxShadow: `none`, },
-    h1: { fontFamily: ["Roboto Slab", "Noto Sans SC", "sans-serif"].join(",") },
-    blockquote: { fontFamily: ["Zilla Slab", "serif"].join(",") },
+    h1: { fontFamily: ["Roboto Slab", SourceHanSans, "sans-serif"].join(",") },
+    blockquote: { fontFamily: ["Zilla Slab", SourceHanSerif, "serif"].join(",") },
     cite: { fontFamily: ["Zilla Slab", "serif"].join(","), fontSize: "1.125em" },
     ".custom-block": { marginBottom: rhythm(1) },
     ".subtitle": { fontFamily: ["Zilla Slab", "serif"].join(","), fontSize: "1.125em" },
@@ -16,16 +22,16 @@ Wordpress2016.overrideThemeStyles = ({ rhythm }, _, styles) => {
 }
 
 Wordpress2016.googleFonts = [
-  { name: "Noto Serif SC", styles: [400, 900] },
-  { name: "Noto Sans SC", styles: [900] },
+  // { name: "Noto Serif SC", styles: [400, 900] },
+  // { name: "Noto Sans SC", styles: [900] },
 ]
 
 Wordpress2016.bodyFontFamily = [
-  "Noto Serif SC", "Georgia", "serif"
+  SourceHanSerif, "Georgia", "serif"
 ]
 
 Wordpress2016.headerFontFamily = [
-  "Roboto Slab", "Noto Sans SC", "sans-serif"
+  "Roboto Slab", SourceHanSans, "sans-serif"
 ]
 
 const typography = new Typography(Wordpress2016)
