161c4738ebefca9933041c859e3663f7999c2361
Yu Jinyan <yujinyan@outlook.com>
2021-03-19 15:39:01 +0800

    debugging

--- src/components/DpTable/DpTable.tsx
+++ src/components/DpTable/DpTable.tsx
@@ -1,9 +1,11 @@
 import React from "react"
-import { formatErrorDetails } from "gatsby/dist/query/utils"
+import "./dp-table.scss"
 
 interface DpTableProp {
   dimension: [number, number],
-  compute: (dp: Array<Array<number>>, i: number, j: number) => number,
+  compute: (dp: Array<Array<number>>) => void,
+  horizontalHeader?: (j) => number,
+  verticalHeader?: (i) => number
 }
 
 export default (props: DpTableProp) => {
@@ -11,27 +13,36 @@ export default (props: DpTableProp) => {
   const n = props.dimension[1]
   const dp = Array(m)
   for (let i = 0; i < n; i++) {
-    dp.push(Array(n))
+    dp[i] = Array(n).fill(0)
   }
-  console.log(m, n)
+
+  console.dir(dp)
+
+  props.compute(dp)
+
   return (
     <table className="dp-table">
       <tbody>
       {
+        props.horizontalHeader &&
+        <tr>
+          {<th />}
+          {[...Array(n)].map((_, j) => <th key={j}>{props.horizontalHeader(j)}</th>)}
+        </tr>
+      }
+      {
         [...Array(m)].map((_, i) =>
           <tr key={i}>
             {
+              props.verticalHeader && <th key={i}>{props.verticalHeader(i)}</th>
+            }
+            {
               [...Array(n)].map((_, j) =>
-                <td key={j}>{props.compute(dp, i, j)}</td>,
+                <td key={j}>{dp[i][j]}</td>,
               )
             }
           </tr>,
         )
-        // [...Array(m)].map(
-        //   (_, i) => (<tr key={i}>
-        //     {[...Array(n).map((_, j) => <td key={j}>{i} - {j}</td>)]}
-        //   </tr>),
-        // )
       }
       </tbody>
     </table>
