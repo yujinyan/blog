499ce6ceceef9bd17ac4be9ab0431b115b88cd82
Yu Jinyan <yujinyan@outlook.com>
2021-04-01 16:05:43 +0800

    Citations.tsx

---
+++ src/components/Citations.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const TheAlgorithmDesignManual =
+  ({ section, chapter }: { section?: [number, string], chapter?: [number, string] }) =>
+    <>
+      Skiena, Steven S..{` `}
+      {section && `“§${section[0]} ${section[1]}.”`}
+      {chapter && `“Chapter ${chapter[0]}. ${chapter[1]}.”`}
+      {` `}<cite>The Algorithm Design Manual</cite>.
+      Switzerland, Springer International Publishing, 2020.
+    </>
