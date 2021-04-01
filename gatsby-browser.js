5b788b6ecf30122a89c8d8d50b71ebd275774b3d
Yu Jinyan <yujinyan@outlook.com>
2021-04-01 16:05:35 +0800

    optimize code .comment font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,6 +8,7 @@ import "@fontsource/roboto-slab/900.css"
 import "@fontsource/jetbrains-mono/400.css"
 import "@fontsource/zilla-slab/400-italic.css"
 import "@fontsource/zilla-slab/400.css"
+import "victormono"
 
 
 // Hot reload typography in development.
