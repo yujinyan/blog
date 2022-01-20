a3e41fae385b5bf366b23a8ec0668fcbbc348a9f
Yu Jinyan <yujinyan@outlook.com>
2022-01-20 23:53:45 +0800

    book status init

--- src/pages/bookshelf.jsx
+++ src/pages/bookshelf.jsx
@@ -8,20 +8,32 @@ import get from "lodash/get"
 
 const BookShelfPage = ({ data, location }) => {
   const books = data.allBook.edges.map((it) => it.node)
+  console.log(books)
+  const booksByStatus = groupBy(books, (book) => book.status)
+  console.log(booksByStatus)
 
   return <Layout location={location}>
-    <div className="flex items-end flex-wrap">
-      {books.map((book) =>
-          <div className="hover:scale-110 hover:z-50 transition-transform">
-            <Link to={get(book, "post.fields.slug")} style={{ textDecoration: "none" }}>
-              <Image fluid={book.coverFile.childImageSharp.fluid} className="rounded shadow-xl xl:w-48 w-36 m-2 border dark:border-gray-500" />
-            </Link>
-          </div>,
-        // <BookColumn {...book} />,
-      )}
-    </div>
+    <Title>已读 | Done</Title>
+    <BookGrid books={booksByStatus.get("done")} />
+    <Title>想读 | Planned</Title>
+    <BookGrid books={booksByStatus.get("planned")} />
   </Layout>
 }
+
+const Title = ({ children }) => (<h2 className="mb-4 mt-8">{children}</h2>)
+
+const BookGrid = ({ books }) => (
+  <div className="flex items-end flex-wrap gap-4">
+    {books.map((book) =>
+      <div className="hover:scale-110 hover:z-50 transition-transform">
+        <Link to={get(book, "post.fields.slug")} style={{ textDecoration: "none" }}>
+          <Image fluid={book.coverFile.childImageSharp.fluid}
+                 className="rounded shadow-xl xl:w-48 w-36 border dark:border-gray-500" />
+        </Link>
+      </div>,
+    )}
+  </div>
+)
 
 export default BookShelfPage
 
@@ -38,6 +50,7 @@ query MyQuery {
         author
         title
         year
+        status
         coverFile {
           childImageSharp {
             fixed {
@@ -56,3 +69,32 @@ query MyQuery {
   }
 }
 `
+
+/**
+ * https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
+ * @description
+ * Takes an Array<V>, and a grouping function,
+ * and returns a Map of the array grouped by the grouping function.
+ *
+ * @param list An array of type V.
+ * @param keyGetter A Function that takes the the Array type V as an input, and returns a value of type K.
+ *                  K is generally intended to be a property key of V.
+ *
+ * @returns Map of the array grouped by the grouping function.
+ */
+//export function groupBy<K, V>(list: Array<V>, keyGetter: (input: V) => K): Map<K, Array<V>> {
+//    const map = new Map<K, Array<V>>();
+function groupBy(list, keyGetter) {
+  const map = new Map()
+  list.forEach((item) => {
+    const key = keyGetter(item)
+    const collection = map.get(key)
+    if (!collection) {
+      map.set(key, [item])
+    } else {
+      collection.push(item)
+    }
+  })
+  return map
+}
+
