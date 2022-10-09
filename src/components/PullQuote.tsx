e98bf9547c3a2e0aeb406b5468882f249c03ff5c
Yu Jinyan <jinyan.yu@ingka.ikea.com>
2022-10-09 21:57:13 +0800

    fix code / pull quote break point size

--- src/components/PullQuote.tsx
+++ src/components/PullQuote.tsx
@@ -3,9 +3,9 @@ import React from "react"
 
 export default function PullQuote({ children, emoji }:
                                     React.PropsWithChildren<{ emoji?: string }>) {
-  return <div className="ml-0 md:-ml-16 mt-12 md:mt-auto relative border-t border-b border-hr">
+  return <div className="ml-0 lg:-ml-16 mt-12 lg:mt-auto relative border-t border-b border-hr">
     {
-      emoji && <div className="absolute ml-0 md:-ml-2 -top-12 text-[2.5rem]">{emoji}</div>
+      emoji && <div className="absolute ml-0 lg:-ml-2 -top-12 text-[2.5rem]">{emoji}</div>
     }
     <div>
       {children}
