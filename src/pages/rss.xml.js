4128a8955ffcb2e8b9f71af5cefda439e8ba0797
Jinyan Yu <jinyan.yu@ingka.ikea.com>
2023-12-22 16:51:36 +0800

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,14 +2,16 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
+export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
-			...post.data,
+			title: post.data.title,
+			description: post.data.description,
+			pubDate: post.data.date,
 			link: `/posts/${post.slug}/`,
 		})),
 	});
